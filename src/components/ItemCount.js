3d7643a581076c219918fbfee4ee87f71754dd79
MatiasGoyaCampanella <goya.utn@gmail.com>
2021-06-08 19:59:31 -0300

    ERROR CONDICIONAL

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -4,11 +4,17 @@ const Contador = () => {
     const [clicks , setClicks] = React.useState(0);
     function aumentarContador () {
         setClicks(clicks + 1);
+        const stock = 10
+        /*if(clicks(10)) {
+            
+            <p>{`Superaste el limete 10 de stock`}</p>
+        }*/
     };
     return (
         <div> 
           <button onClick = {aumentarContador}>Contador</button> 
           <p>{`Clickeaste ${clicks} veces`}</p>
+          <small>{new Date().toLocaleString()}</small>
         </div>
     )
 
