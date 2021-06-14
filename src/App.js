d4e9b549036850758c735489f4e21824491b48ef
MatiasGoyaCampanella <goya.utn@gmail.com>
2021-06-14 14:58:33 -0300

    Promises

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 import NavBar from './components/NavBar.js';
 import ItemListContainer from './components/ItemListContainer.js';
 import ItemCount from './components/ItemCount.js';
+import Item from './components/Item.js';
+import ItemList from './components/ItemList.js';
+import MyButton from './components/MyButton.js';
+
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return ( <div className = "App" >  
     <NavBar/>
     <ItemCount/>
+    <MyButton/>
     <ItemListContainer greeting = "Productos"/>
      </div>);
 }
---
+++ src/components/MyButton.js
@@ -0,0 +1,50 @@
+import react from 'react';
+export default function() {
+
+    const carrito = [
+        { id: "A-01", name:"Perfume LG", description: "Lorem", stock:6},
+        { id: "A-02", name:"Perfume LG", description: "Lorem", stock:6},
+        { id: "B-01", name:"Crema LG", description: "Lorem", stock:6},
+        { id: "B-01", name:"Crema LG", description: "Lorem", stock:6},
+        { id: "C-01", name:"Maquillaje LG", description: "Lorem", stock:6},
+        { id: "C-01", name:"Maquillaje LG", description: "Lorem", stock:6},
+        { id: "D-01", name:"otros LG", description: "Lorem", stock:6},
+        { id: "D-01", name:"otros LG", description: "Lorem", stock:6}
+    ];
+    function getCartItems() {
+        let miPromesa = new Promise ((resolve, reject) => {
+            //Reject (Cuando no pudimos realizar la tarea o hibo un error)
+            //con un numero aleatorio simulamos un error en la request
+
+            //Simulamos una funcion "Asincrona" usando setTimeout
+            setTimeout(function() {
+                const error = Math.random() > 0.85;
+                //resolve(se ejecuta cuando la tarea se cumple con exito)
+                if(!error) {
+                    resolve(carrito);
+                }
+                reject("Error obteniendo datos");
+
+            }, 500);
+
+        });
+        miPromesa.then(function(resultado){
+            console.log(resultado);
+        }).catch(
+            function(error) {
+                console.log(error);
+            }
+        ).finally(
+            function() {
+                alert('Promesa terminada')
+            }
+        )
+
+    }
+
+    return (
+        <div>
+            <button onClick={getCartItems}>Ver mi Carrito</button>
+        </div>
+    )
+}
