ee7e1f8d60a0dcbc2c33f922e772128aef14e634
MatiasGoyaCampanella <goya.utn@gmail.com>
2021-07-12 16:39:16 -0300

    Button size

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -5,18 +5,18 @@ function Counter(props) {
   return (
     <div className="d-flex justify-content-center m-4">
       <Button
-        style={{ width: '2rem' }}
+        style={{ width: '4rem' }}
         variant="danger"
         onClick={props.subItem}
       >
         -
       </Button>
-      <div className="m-3" style={{ width: '2rem' }}>
+      <div className="m-3" style={{ width: '4rem' }}>
         {props.counter}
       </div>
 
       <Button
-        style={{ width: '2rem' }}
+        style={{ width: '4rem' }}
         variant="success"
         onClick={props.addItem}
       >
